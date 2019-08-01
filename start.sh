4f837e5d743bb91ed10bb4de540c25f315f20e86
emmanuelmuchiri <emmanuel.muchiri@outlook.com>
2019-08-01 15:19:49 +0300

    deployment

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 export NEWS_API_KEY='81f85992543b43e5ad87e6c37c018432'
 export SECRET_KEY='saniimuthui'
 
-python3.7 manage.py server
+python3.6 manage.py server
+
+#  heroku config:set NEWS_API_KEY='81f85992543b43e5ad87e6c37c018432' && heroku config:set SECRET_KEY='saniimuthui'
